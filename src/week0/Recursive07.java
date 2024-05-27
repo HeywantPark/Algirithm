c9411fac658ed224be507361de00aeb81a892b71
Park Hyewon <db1648@naver.com>
2024-05-27 11:40:00 +0900

    240527 재귀 알고리즘 7 powerSet

--- src/week0/Recursive07.java
+++ src/week0/Recursive07.java
@@ -1,32 +1,34 @@
 package week0;
 
-import java.util.Arrays;
-
 public class Recursive07 {
-    static int arr[] = { 1, 3, 5 };
+    static int[] arr = { 1, 3, 5 };
 
     public static void main(String[] args) {
-        // 부분집합의 집합 powerSet
-        recursive(0,0,new boolean[arr.length]);
-
-
+        // 부분집합 구하기
+        boolean[] sel = new boolean[arr.length];
+        recursive(0, sel);
     }
-    public static void recursive(int idx,int size,boolean[] sel) {
+
+    public static void recursive(int idx, boolean[] sel) {
         // basis part
         if (idx == sel.length) {
-            for (int i = 0; i< sel.length; i++) {
+            // 선택된 요소들을 출력
+            for (int i = 0; i < sel.length; i++) {
                 if (sel[i]) {
-                    System.out.println(arr[i] + " ");
+                    System.out.print(arr[i] + " ");
                 }
             }
             System.out.println();
             return;
-
         }
+
         // inductive part
+        // 현재 요소를 선택하는 경우
         sel[idx] = true;
-        recursive(idx+1,size+1,sel);
+        recursive(idx + 1, sel);
+
+        // 현재 요소를 선택하지 않는 경우
         sel[idx] = false;
-        recursive(idx+1,size,sel);
+        recursive(idx + 1, sel);
     }
 }
