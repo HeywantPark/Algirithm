a1590e9a358491e2fa8ef37cbefb10dca0d23aca
Park Hyewon <db1648@naver.com>
2024-07-07 01:04:05 +0900

    240707 완주하지 못한 선수 HashMap을 활용한 풀이

--- src/week1/participant/Solution.java
+++ src/week1/participant/Solution.java
@@ -1,4 +1,28 @@
 package week1.participant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    public String disComplete(String[] participant, String[] completion) {
+        // 해시맵을 사용한 풀이
+        Map<String, Integer> counter = new HashMap<>();
+        for (String p : participant) {
+            counter.put(p, counter.getOrDefault(p, 0) + 1);
+        }
+        for (String c : completion) {
+            counter.put(c, counter.getOrDefault(c, 0) - 1);
+        }
+        for (String k : counter.keySet()) {
+            if (counter.get(k) != 0) {
+                return k;
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.disComplete(new String[] {"marina", "josipa", "nikola", "vinko", "filipa"}, new String[] {"josipa", "filipa", "marina", "nikola"});
+        System.out.println(result);
+    }
 }
