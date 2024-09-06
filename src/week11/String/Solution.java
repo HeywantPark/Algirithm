87611fc1bfad1a2f43b09050bd042ac2f59df5bc
Park Hyewon <db1648@naver.com>
2024-09-06 23:36:09 +0900

    240906 문자열 관련 문제 풀이

---
+++ src/week11/String/Solution.java
@@ -0,0 +1,14 @@
+package week11.String;
+
+public class Solution {
+    boolean solution(String s) {
+        int pcount = 0;
+        int ycount = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P') pcount++;
+            if(s.charAt(i) == 'y' || s.charAt(i) == 'Y') ycount++;
+        }
+        return pcount == ycount;
+    }
+}
