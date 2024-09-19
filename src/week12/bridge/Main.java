0396a5cee0c60a2e8f7076165605db1f0d2b939d
Park Hyewon <db1648@naver.com>
2024-09-19 23:00:46 +0900

    240919 알고리즘 다리놓기 문제 풀이 주석 첨가

--- src/week12/bridge/Main.java
+++ src/week12/bridge/Main.java
@@ -7,28 +7,31 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int t = sc.nextInt();
+        int t = sc.nextInt();  // 테스트 케이스 개수 입력
 
         for (int i = 0; i < t; i++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
+            int n = sc.nextInt();  // 서쪽 사이트 개수 (N)
+            int m = sc.nextInt();  // 동쪽 사이트 개수 (M)
 
-            int[][] site = new int[m + 1][n + 1];
+            int[][] site = new int[m + 1][n + 1];  // DP 배열 초기화
 
-            //basecase
+            // 기본 케이스 설정
             for (int j = 0; j <= m; j++) {
-                site[j][0] = 1;
+                site[j][0] = 1;  // 다리 0개를 놓는 경우
                 if(j <= n) {
-                    site[j][j] = 1;
+                    site[j][j] = 1;  // 다리 개수와 사이트 개수가 같을 때
                 }
             }
-            //dp 점화식 사용
+
+            // DP 점화식
             for (int j = 1; j <= m; j++) {
                 for (int k = 1; k <= n; k++) {
-                    site[j][k] = site[j-1][k-1] + site[j-1][k];
+                    site[j][k] = site[j-1][k-1] + site[j-1][k];  // 조합 계산
                 }
             }
-            System.out.println(site[m][n]);
+
+            System.out.println(site[m][n]);  // 결과 출력
         }
+        sc.close();
     }
 }
