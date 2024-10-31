8e3e90fd116f0f11dad3a71e1a78eb0474b94c75
Park Hyewon <db1648@naver.com>
2024-10-31 14:18:06 +0900

    241031 유턴싫어 알고리즘 문제 완전탐색 풀이

--- src/week16/nouturn/Main.java
+++ src/week16/nouturn/Main.java
@@ -1,63 +1,48 @@
 package week16.nouturn;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static char[][] board;
-    static int r, c;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-        // 1. 문제의 input을 받습니다.
-        String[] input = br.readLine().split(" ");
-        r = Integer.parseInt(input[0]);
-        c = Integer.parseInt(input[1]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        board = new char[r][c];
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        char[][] village = new char[r][c];
+
+        // 마을 지도 입력
         for (int i = 0; i < r; i++) {
-            board[i] = br.readLine().toCharArray();
+            String line = sc.next();
+            village[i] = line.toCharArray();
         }
-
-        // 3. 지도 위의 모든 길에서 연결된 길의 개수를 구해 막다른 길인지 판단합니다.
-        boolean needUTurn = false;
-        outerLoop:
+        // 막다른 길 인지 탐색
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (board[i][j] == '.') {
-                    if (calcWays(i, j) <= 1) {
-                        needUTurn = true;
-                        break outerLoop;
+                if (village[i][j] == '.') {
+                    int pathCount = 0;
+
+                    for (int k = 0; k < 4; k++) {
+                        int nr = i + dr[k];
+                        int nc = j + dc[k];
+
+                        if (nr >= 0 && nr < r && nc >= 0 && nc < c && village[nr][nc] == '.') {
+                            pathCount++;
+                        }
+                    }
+                    // 길이 1개 이하라면 막다른 길
+                    if (pathCount <= 1) {
+                        System.out.println(1);
+                        return;
                     }
                 }
-            }
-        }
 
-        // 결과 출력
-        if (needUTurn) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }
-    }
-
-    // 2. 막다른 길을 탐색하기 위해 연결된 길의 개수를 구하는 함수를 구현합니다.
-    static int calcWays(int x, int y) {
-        int way = 0;
-        for (int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
-                continue;
-            }
-            if (board[nx][ny] == '.') {
-                way++;
             }
         }
-        return way;
+        System.out.println(0);
     }
 }
