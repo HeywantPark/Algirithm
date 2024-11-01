bb40efc72b71abfade31521c320fcc445cf3bd72
Park Hyewon <db1648@naver.com>
2024-11-02 01:31:09 +0900

    241002 숫자카드 알고리즘 이분탐색(HashSet) 풀이

---
+++ src/week16/numbercard/Main.java
@@ -0,0 +1,27 @@
+package week16.numbercard;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int num = sc.nextInt();
+            if (set.contains(num)) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+}
