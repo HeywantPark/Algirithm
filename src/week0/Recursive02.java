b0f332b74a49becb14f3a6e90ba01303e18ffe83
Park Hyewon <db1648@naver.com>
2024-11-07 00:47:33 +0900

    240522 재귀 알고리즘 개념 2

---
+++ src/week0/Recursive02.java
@@ -0,0 +1,17 @@
+package week0;
+
+public class Recursive02 {
+    public static void main(String[] args) {
+        // 1 ~ 3 더한값과  곱한값을 구하세요
+        recursive(1,0,1);
+    }
+    public static void recursive(int idx, int sum, int cross) {
+        // basis part
+        if (idx > 5) {
+            System.out.println("sum = " + sum + ", cross = " + cross);
+            return;
+        }
+        // inductive part
+        recursive(idx+1, sum+idx, cross*idx);
+    }
+}
